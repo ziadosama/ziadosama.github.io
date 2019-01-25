dfb01cd5b95f635325aa4e09adea9159dbef07fd
unknown <ziadosama@aucegypt.edu>
2019-01-25 17:54:29 +0200

    Updates

---
+++ precache-manifest.4b6c3d8e8dea0cef7a7050b1eafce6de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2891fe23f330bdd399eec9ec940514e",
+    "url": "/static/media/logo.d2891fe2.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d958e6b9c7a24c9b77c7",
+    "url": "/static/js/main.d958e6b9.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d958e6b9c7a24c9b77c7",
+    "url": "/static/css/main.c836b56f.chunk.css"
+  },
+  {
+    "revision": "8058ac6d77c4bff628bb000a2d17f324",
+    "url": "/index.html"
+  }
+];
