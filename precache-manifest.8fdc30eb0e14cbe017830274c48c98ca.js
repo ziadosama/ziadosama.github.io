bae3d563850f28bdbd0b188371a5d34b786cf987
unknown <ziadosama@aucegypt.edu>
2019-02-08 05:13:45 +0200

    Updates

---
+++ precache-manifest.8fdc30eb0e14cbe017830274c48c98ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1175b8447f8a452c660f",
+    "url": "/static/js/main.1175b844.chunk.js"
+  },
+  {
+    "revision": "b7d5802b3cefd9b458d0",
+    "url": "/static/js/1.b7d5802b.chunk.js"
+  },
+  {
+    "revision": "1175b8447f8a452c660f",
+    "url": "/static/css/main.2b51dc58.chunk.css"
+  },
+  {
+    "revision": "adb847100d61ef46f0051e6dd751246a",
+    "url": "/index.html"
+  }
+];
